21aacc313511a615e091c73a0a8dac68041a47e7
philopian <pdotwdot@gmail.com>
2021-01-15 08:47:13 -0800

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Phil Dev Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Phil Willis`,
+      summary: `who lives and works in Portland, Oregon building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `Just a bunch of stuff i've picked up along the way`,
+    siteUrl: `https://blog.phil-willis.com`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `pdotdotdot`,
     },
   },
   plugins: [
@@ -61,8 +61,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Phil Dev Blog`,
+        short_name: `PhilDevBlog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
